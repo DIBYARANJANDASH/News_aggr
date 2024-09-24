853bea60b8eba97702f4ad7433538dfab163921a
DIBYARANJANDASH <dash.dibya007@gmail.com>
2024-09-24 14:00:53 +0530

    Added Dependencies

---
+++ src/main/java/com/newsagg_nlp/news_agg/NewsAggApplication.java
@@ -0,0 +1,13 @@
+package com.newsagg_nlp.news_agg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsAggApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsAggApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/newsagg_nlp/news_agg/NewsAggApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newsagg_nlp.news_agg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsAggApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
