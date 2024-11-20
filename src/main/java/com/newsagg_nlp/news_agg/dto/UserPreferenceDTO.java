a5935e03fccae1e073e20d9fd5f9e64bba9a060f
Mahesh <sahu69675@gmail.com>
2024-11-20 18:03:47 +0530

    Added custom priority can be set by user

--- src/main/java/com/newsagg_nlp/news_agg/dto/UserPreferenceDTO.java
+++ src/main/java/com/newsagg_nlp/news_agg/dto/UserPreferenceDTO.java
@@ -3,14 +3,16 @@ package com.newsagg_nlp.news_agg.dto;
 public class UserPreferenceDTO {
     private String categoryName;
     private String subCategoryName;
+    private String subCategoryId;
     private int priority;
 
     public UserPreferenceDTO() {
     }
 
-    public UserPreferenceDTO(String categoryName, String subCategoryName, int priority) {
+    public UserPreferenceDTO(String categoryName, String subCategoryName, String subCategoryId, int priority) {
         this.categoryName = categoryName;
         this.subCategoryName = subCategoryName;
+        this.subCategoryId = subCategoryId;
         this.priority = priority;
     }
 
@@ -28,6 +30,14 @@ public class UserPreferenceDTO {
 
     public void setSubCategoryName(String subCategoryName) {
         this.subCategoryName = subCategoryName;
+    }
+
+    public String getSubCategoryId() {
+        return subCategoryId;
+    }
+
+    public void setSubCategoryId(String subCategoryId) {
+        this.subCategoryId = subCategoryId;
     }
 
     public int getPriority() {
